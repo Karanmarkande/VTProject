cb8119086e86556d0a4be44495d99b530929cfa9
Piyush0369 <piyush0369@gmail.com>
2024-08-11 12:07:11 +0530

    removed finish from Splash

--- app/src/main/java/com/upu/vtproject/ui/activity/SplashActivity.java
+++ app/src/main/java/com/upu/vtproject/ui/activity/SplashActivity.java
@@ -29,7 +29,7 @@ public class SplashActivity extends AppCompatActivity {
                 } else {
                     Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                     startActivity(intent);
-                    finish();
+
                 }
 
             }
